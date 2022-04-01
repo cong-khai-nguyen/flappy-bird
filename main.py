9fddbeecbfde4ae3be2216cdc67dfddfab64752d
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-01 15:23:44 -0500

    set up Bird class

--- main.py
+++ main.py
@@ -12,3 +12,26 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("media", "b
 PIPE_IMG  = pygame.transform.scale2x(pygame.image.load(os.path.join("media", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("media", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("media", "bg.png")))
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
