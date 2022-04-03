ba73aeb2090f224c076f47613ac2ec736be2ca1c
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-03 09:19:06 -0500

    add a 'Pipe' class with different methods to set up the pipe on screen

--- main.py
+++ main.py
@@ -79,6 +79,39 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
+
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __int__(self, x):
+        self.x = x
+        self.height = 0
+        self.gapp = 100
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self):
+        pass
+
+
+
+
+
 def draw_window(win, bird):
     win.blit(BG_IMG, (0,0))
     bird.draw(win)
